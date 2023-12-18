662d78c105137a058306f7f23e46255887d3693d
paivabenja <paivabenja05@gmail.com>
2023-12-18 04:52:22 -0300

    añadir opciones en magna

--- index.js
+++ index.js
@@ -9,7 +9,41 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
-const hiddenElements = document.querySelectorAll(".hidden");
-hiddenElements.forEach((element) => observer.observe(element));
+const prevToAnimationElements = document.querySelectorAll(".prevToAnimation");
+prevToAnimationElements.forEach((element) => observer.observe(element));
 
-// document.addEventListener("scroll", () => console.log("scroll"));
+const options = document.getElementById("options");
+const magna = document.getElementById("magna");
+const laburoHead = document.getElementById("laburo-head");
+
+const showOptions = () => {
+  magna.style.paddingLeft = 0;
+  magna.style.paddingRight = 0;
+  magna.style.paddingBottom = 0;
+  options.style.marginLeft = 0;
+  options.style.marginRight = 0;
+  options.style.marginBottom = 0;
+  options.classList.remove("hidden");
+  laburoHead.style.marginLeft = "3rem";
+  laburoHead.style.marginRight = "3rem";
+};
+
+const hideOptions = () => {
+  magna.style.paddingLeft = "3rem";
+  magna.style.paddingRight = "3rem";
+  magna.style.paddingBottom = "3rem";
+  options.style.marginLeft = "3rem";
+  options.style.marginRight = "3rem";
+  options.style.marginBottom = "3rem";
+  options.classList.add("hidden");
+  laburoHead.style.marginLeft = 0;
+  laburoHead.style.marginRight = 0;
+};
+
+magna.onclick = () => {
+  if (options.classList.contains("hidden")) {
+    showOptions();
+  } else {
+    hideOptions();
+  }
+};
